140d84b6d2556b077c0d8f2b3f8e0d2870302d6e
sukreshmanda <34400639+sukreshmanda@users.noreply.github.com>
2020-12-05 20:28:37 +0530

    Create template.js

---
+++ Creating Modules/template.js
@@ -0,0 +1,10 @@
+var url = 'https://sukreshphoto.blogspot.com';
+
+function log(message){
+   //Send http request
+   console.log(message);
+}
+// To make it visible to outside simply export it
+
+module.exports.log = log;
+module.exports.siteUrl = url;
